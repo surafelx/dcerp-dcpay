f948f2477d7a07dd42a18ff30e2da621f6729a2d
unknown <surafelyimam.sy@gmail.com>
2023-06-17 12:29:28 +0300

    Data-Updator Deployment

--- data-updator/data/initial.sql
+++ data-updator/data/initial.sql
@@ -1,3 +1,71 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
+-- Drop table 'tax_rate'
+DROP TABLE IF EXISTS tax_rate;
 
+-- Drop table 'transaction_calculation'
+DROP TABLE IF EXISTS transaction_calculation;
+
+-- Drop table 'discontinuation'
+DROP TABLE IF EXISTS discontinuation;
+
+-- Drop table 'membership'
+DROP TABLE IF EXISTS membership;
+
+-- Drop table 'pay_transaction'
+DROP TABLE IF EXISTS pay_transaction;
+
+-- Drop table 'loan_transaction'
+DROP TABLE IF EXISTS loan_transaction;
+
+-- Drop table 'transaction_definition'
+DROP TABLE IF EXISTS transaction_definition;
+
+-- Drop table 'employee_bank_account'
+DROP TABLE IF EXISTS employee_bank_account;
+
+-- Drop table 'employee'
+DROP TABLE IF EXISTS employee;
+
+-- Drop table 'parameter_definition'
+DROP TABLE IF EXISTS parameter_definition;
+
+-- Drop table 'department'
+DROP TABLE IF EXISTS department;
+
+-- Drop table 'navigation'
+DROP TABLE IF EXISTS navigation;
+
+-- Drop table 'roles_rights'
+DROP TABLE IF EXISTS roles_rights;
+
+-- Drop table 'user_rights'
+DROP TABLE IF EXISTS user_rights;
+
+-- Drop table 'role_menu'
+DROP TABLE IF EXISTS role_menu;
+
+-- Drop table 'role_branch'
+DROP TABLE IF EXISTS role_branch;
+
+-- Drop table 'role_user'
+DROP TABLE IF EXISTS role_user;
+
+-- Drop table 'holiday'
+DROP TABLE IF EXISTS holiday;
+
+-- Drop table 'user_accounts'
+DROP TABLE IF EXISTS user_accounts;
+
+-- Drop table 'user_roles'
+DROP TABLE IF EXISTS user_roles;
+
+-- Drop table 'branch'
+DROP TABLE IF EXISTS branch;
+
+-- Drop table 'periods'
+DROP TABLE IF EXISTS periods;
+
+-- Drop table 'menu_items'
+DROP TABLE IF EXISTS menu_items;
+
+-- Drop table 'organizations'
+DROP TABLE IF EXISTS organizations;
