6605cdb32191339c00cfab5e2dc627225f7a757e
unknown <surafelyimam.sy@gmail.com>
2023-06-18 14:51:38 +0300

    Fixing Build Errors

--- dashboard/src/pages/apps/file/period/index.tsx
+++ dashboard/src/pages/apps/file/period/index.tsx
@@ -1,62 +1,39 @@
 // ** React Imports
-import { useState, useEffect, MouseEvent } from 'react'
-
-// ** Next Import
-import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import Menu from '@mui/material/Menu'
+import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Grid'
 import { DataGrid } from '@mui/x-data-grid'
-import MenuItem from '@mui/material/MenuItem'
-import { styled } from '@mui/material/styles'
-import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
 
 // ** Store Imports
 import { useDispatch, useSelector } from 'react-redux'
 
-
 // ** Actions Imports
-import { fetchData, deletePeriod } from 'src/store/apps/File/Period'
+import { fetchData } from 'src/store/apps/File/Period'
 
 // ** Types Imports
 import { RootState, AppDispatch } from 'src/store'
 import { PeriodType } from 'src/types/apps/File/periodTypes'
-
-
 import AddPeriod from 'src/views/dc-pay/forms/File/Period/AddPeriod'
-
-
-
 
 
 interface CellType {
     row: PeriodType
 }
 
-
-// ** Styled component for the link inside menu
-const MenuItemLink = styled('a')(({ theme }) => ({
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    textDecoration: 'none',
-    padding: theme.spacing(1.5, 4),
-    color: theme.palette.text.primary
-}))
-
 const UserList = () => {
+    
     // ** State
     const [role] = useState<string>('')
     const [value] = useState<string>('')
     const [status] = useState<string>('')
     const [pageSize, setPageSize] = useState<number>(10)
 
-    const [formData, setFormData] = useState({
+    const [formData, ] = useState({
         id: '',
         firstTransaction: '',
         secondTransaction: '',
@@ -67,98 +44,6 @@ const UserList = () => {
         rate: ''
     });
 
-
-    const RowOptions = ({ id,
-        firstTransaction,
-        secondTransaction,
-        thirdTransaction,
-        calculationUnit,
-        firstOption,
-        secondOption,
-        rate }: any) => {
-        // ** Hooks
-        const dispatch = useDispatch<AppDispatch>()
-
-        // ** State
-        const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-
-        const rowOptionsOpen = Boolean(anchorEl)
-
-        const handleRowOptionsClick = (event: MouseEvent<HTMLElement>) => {
-            setAnchorEl(event.currentTarget)
-        }
-        const handleRowOptionsClose = () => {
-            setAnchorEl(null)
-        }
-
-        const handleEdit = () => {
-            setFormData(
-                {
-                    id,
-                    firstTransaction,
-                    secondTransaction,
-                    thirdTransaction,
-                    calculationUnit,
-                    firstOption,
-                    secondOption,
-                    rate
-                }
-            )
-        }
-
-
-        useEffect(() => {
-            if (formData) {
-                setFormData(formData);
-            }
-        }, []);
-
-        const handleDelete = () => {
-            dispatch(deletePeriod(id))
-            handleRowOptionsClose()
-        }
-
-        return (
-            <>
-                <IconButton size='small' onClick={handleRowOptionsClick}>
-                    {/* <DotsVertical /> */}
-                    Options
-                </IconButton>
-                <Menu
-                    keepMounted
-                    anchorEl={anchorEl}
-                    open={rowOptionsOpen}
-                    onClose={handleRowOptionsClose}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'right'
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right'
-                    }}
-                    PaperProps={{ style: { minWidth: '8rem' } }}
-                >
-                    <MenuItem sx={{ p: 0 }}>
-                        <Link href={`/apps/settings/user-management/view/${id}`} passHref>
-                            <MenuItemLink>
-                                {/* <EyeOutline fontSize='small' sx={{ mr: 2 }} /> */}
-                                View
-                            </MenuItemLink>
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={handleEdit}>
-                        {/* <PencilOutline fontSize='small' sx={{ mr: 2 }} /> */}
-                        Edit
-                    </MenuItem>
-                    <MenuItem onClick={handleDelete}>
-                        {/* <DeleteOutline fontSize='small' sx={{ mr: 2 }} /> */}
-                        Delete
-                    </MenuItem>
-                </Menu>
-            </>
-        )
-    }
 
     const columns = [
         {
