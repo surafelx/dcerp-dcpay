624da66450a3dc91372cca8d6facb27d7c98a880
unknown <surafelyimam.sy@gmail.com>
2023-08-12 15:55:00 +0300

    Uncommented

--- dashboard/src/layouts/UserLayout.tsx
+++ dashboard/src/layouts/UserLayout.tsx
@@ -10,8 +10,8 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import Layout from 'src/@core/layouts/Layout'
 
 // ** Navigation Imports
-import VerticalNavItems from 'src/navigation/vertical'
-import HorizontalNavItems from 'src/navigation/horizontal'
+// import VerticalNavItems from 'src/navigation/vertical'
+// import HorizontalNavItems from 'src/navigation/horizontal'
 
 // ** Component Import
 // Uncomment the below line (according to the layout type) when using server-side menu
